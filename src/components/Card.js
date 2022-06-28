3c962529bb396fe5321100833aa01e6f5d941dcd
Mikeheul <mickael.murmann@gmail.com>
2022-06-28 19:33:51 +0200

    css

--- src/components/Card.js
+++ src/components/Card.js
@@ -95,34 +95,35 @@ const Card = ({ movie }) => {
 
   return (
     <div className="card">
-      <img
-        src={
-          movie.poster_path
-            ? "https://image.tmdb.org/t/p/w500" + movie.poster_path
-            : "./img/poster.jpg"
-        }
-        alt="affiche film"
-      />
-      <h2>{movie.title}</h2>
-      {movie.release_date ? (
-        <h5>Sorti le : {dateFormater(movie.release_date)}</h5>
-      ) : (
-        ""
-      )}
-      <h4>
-        {movie.vote_average}/10 <span>⭐</span>
-      </h4>
+      <div>
+        <img
+          src={
+            movie.poster_path
+              ? "https://image.tmdb.org/t/p/w500" + movie.poster_path
+              : "./img/poster.jpg"
+          }
+          alt="affiche film"
+        />
+        <h2>{movie.title}</h2>
+        {movie.release_date ? (
+          <h5>Sorti le : {dateFormater(movie.release_date)}</h5>
+        ) : (
+          ""
+        )}
+        <h4>
+          {movie.vote_average}/10 <span>⭐</span>
+        </h4>
 
-      <ul>
-        {movie.genre_ids
-          ? genreFinder()
-          : movie.genres.map((genre, index) => (
-              <li key={index}>{genre.name}</li>
-            ))}
-      </ul>
-      {movie.overview ? <h3>Synopsis</h3> : ""}
-      <p>{movie.overview}</p>
-
+        <ul>
+          {movie.genre_ids
+            ? genreFinder()
+            : movie.genres.map((genre, index) => (
+                <li key={index}>{genre.name}</li>
+              ))}
+        </ul>
+        {movie.overview ? <h3>Synopsis</h3> : ""}
+        <p>{movie.overview}</p>
+      </div>
       {movie.genre_ids ? (
         <div className="btn" onClick={() => addStorage()}>
           Ajouter aux coups de coeur
