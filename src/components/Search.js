9e1f36655e345da2812c32c474ef9ac3b35cea70
Mikeheul <mickael.murmann@gmail.com>
2022-06-28 15:14:51 +0200

    favorites

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,31 +5,43 @@ import Card from './Card';
 const Search = () => {
 
     const [moviesData, setMoviesData] = useState([])
-    const [searchInput, setSearchInput] = useState("")
+    const [searchInput, setSearchInput] = useState("star")
+    const [sortGoodBad, setSortGoodBad] = useState(null);
 
     useEffect(() => {
         axios
-            .get("https://api.themoviedb.org/3/search/movie?api_key=68ec6963adbf4d67b88cac0414bd2d2d&query=sta&language=fr-FR")
-            .then((res) => setMoviesData(res.data.results))
-    }, [])  // [} pour ne jouer le useEffect qu'une seule fois]
+        .get(`https://api.themoviedb.org/3/search/movie?api_key=68ec6963adbf4d67b88cac0414bd2d2d&query=${searchInput}&language=fr-FR`)
+        .then((res) => setMoviesData(res.data.results))
+    }, [searchInput])  // [} pour ne jouer le useEffect qu'une seule fois]
      
     return (
         <div className="form-component">
             <div className="form-container">
                 <form>
-                    <input type="text" id="searchInput" placeholder="Entrez le titre d'un film" />
+                    <input 
+                        type="text"  
+                        placeholder="Entrez le titre d'un film" 
+                        id="search-input" 
+                        onChange={(e) => setSearchInput(e.target.value)}
+                     />
                     <input type="submit" value="Rechercher" />
                 </form>
 
                 <div className="btn-sort-container">
-                    <div className="btn-sort" id="goodToBad">Top <span>→</span></div>
-                    <div className="btn-sort" id="badToGood">Flop <span>←</span></div>
+                    <div className="btn-sort" id="goodToBad" onClick={() => setSortGoodBad("goodToBad")}>Top <span>→</span></div>
+                    <div className="btn-sort" id="badToGood" onClick={() => setSortGoodBad("badToGood")}>Flop <span>←</span></div>
                 </div>
             </div>
             <div className="result">
                 {
                     moviesData
-                        .slice((0,12))
+                        .sort((a, b) => {
+                            if (sortGoodBad === "goodToBad") {
+                            return b.vote_average - a.vote_average;
+                            } else if (sortGoodBad === "badToGood") {
+                            return a.vote_average - b.vote_average;
+                            }
+                        })
                         .map((movie, index) => (
                         <Card key={index} movie={movie} />
                     ))
--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import Header from '../components/Header';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card";
+import Header from "../components/Header";
 
 const UserList = () => {
-    return (
-        <div>
-            <Header />
-            <h1>User List</h1>
-        </div>
-    );
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    let moviesId = window.localStorage.movies
+      ? window.localStorage.movies.split(",")
+      : [];
+
+    for (let i = 0; i < moviesId.length; i++) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=68ec6963adbf4d67b88cac0414bd2d2d&language=fr-FR`
+        )
+        .then((res) => setListData((listData) => [...listData, res.data]));
+    }
+  }, []);
+
+  return (
+    <div className="user-list-page">
+      <Header />
+      <h2>
+        Coup de coeur <span>💖</span>
+      </h2>
+      <div className="result">
+        {listData.length > 0 ? (
+          listData.map((movie) => <Card movie={movie} key={movie.id} />)
+        ) : (
+          <h2>Aucun coup de coeur pour le moment</h2>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default UserList;
